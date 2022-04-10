7a8b54646af36661323fec5288692316a96ce5bf
BUKODONOZOR <90481288+BUKODONOZOR@users.noreply.github.com>
2022-04-09 20:21:56 -0500

    Ejercicio número 1
    Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los dos. Si son iguales indicarlo también. Ve cambiando los valores para comprobar que funciona.

---
+++ 1/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Double number1 = 1.0;
+        Double number2 = 2.0;
+
+        if (number1 > number2) {
+            System.out.println("La variable número 1 es mayor a la varible número 2");
+        } 
+
+        if (number2 < number1) {
+            System.out.println("La variable número 2 es mayor a la varible número 1");
+        }
+
+        if (number1 == number2) {
+            System.out.println("La variable número 1 y la varible número 2 son iguales"); 
+        }
+    }
+}
